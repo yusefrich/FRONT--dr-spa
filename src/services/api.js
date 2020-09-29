f51d447a95e2e6f46237c9b6787c163766559171
Yusef Richard <yusef.rick@gmail.com>
2020-09-29 15:34:43 -0300

    ajsute de rota

--- src/services/api.js
+++ src/services/api.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 */
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'http://99.79.191.3:3333'
 });
 
 api.interceptors.request.use(async config => {
