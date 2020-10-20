8cf3d3c16aa5d9254d201babf794af266e4b7603
Yusef Richard <yusef.rick@gmail.com>
2020-10-20 12:57:14 -0300

    ajuste rota de api

--- src/services/api.js
+++ src/services/api.js
@@ -7,7 +7,7 @@ import axios from 'axios';
 */
 
 const api = axios.create({
-    baseURL: 'http://localhost:3333'
+    baseURL: 'https://api.drspa.gq'
 });
 
 api.interceptors.request.use(async config => {
